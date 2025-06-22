3d61379f701b4d8aa0224f1a8f67d46bab46f948
jsprice87 <justinstephenprice@gmail.com>
2025-06-22 10:30:39 -0600

    fix: specialise DateInput wrapper to Date | null
    Include updated package.json & pnpm-lock.yaml

--- src/components/mantine/DateInput.tsx
+++ src/components/mantine/DateInput.tsx
@@ -1,13 +1,17 @@
-
-import { DateInput as MantineDateInput, DateInputProps as MantineDateInputProps } from '@mantine/dates';
+import {
+  DateInput as MantineDateInput,
+  DateInputProps as MantineBaseProps,
+} from '@mantine/dates';
 import { forwardRef } from 'react';
 
-export type DateInputProps = MantineDateInputProps<Date | null>;
+export interface DateInputProps
+  extends Omit<MantineBaseProps, 'value' | 'onChange'> {
+  value: Date | null;
+  onChange: (value: Date | null) => void;
+}
 
 export const DateInput = forwardRef<HTMLInputElement, DateInputProps>(
-  (props, ref) => {
-    return <MantineDateInput ref={ref} {...props} />;
-  }
+  (props, ref) => <MantineDateInput ref={ref} {...(props as MantineBaseProps)} />
 );
 
 DateInput.displayName = 'DateInput';
